11e564a82ddc3a23f54473f0bd42fd2eb7551b38
Vinay288 <vinayhiremath288@gmail.com>
2021-11-02 12:45:53 +0530

    ADDED: various http methods

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
@@ -0,0 +1,43 @@
+package com.bridgelabz.employeepayrollapp.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bridgelabz.employeepayrollapp.model.Employee;
+
+@RestController
+public class EmployeePayrollController {
+	@RequestMapping(value = { "", "/", "/home" })
+	public ResponseEntity<String> sayHello() {
+		return new ResponseEntity<String>("Hello world!", HttpStatus.OK);
+	}
+
+	@RequestMapping(value = { "/query" })
+	public String sayHi(@RequestParam(value = "fname", defaultValue = "vinay") String fname,
+			@RequestParam(value = "lname", defaultValue = "Hiremath") String lname) {
+		return "welcome to the world of " + fname + " " + lname;
+	}
+
+	@GetMapping("/param/{name}")
+	public String parameterName(@PathVariable String name) {
+		return "Hi " + name;
+	}
+
+	@GetMapping("/post")
+	public String setUser(@RequestBody Employee employee) {
+		return "Hello " + employee.getFirstName() + " " + employee.getLastName();
+	}
+
+	@PutMapping("/put/{firstName}")
+	public String sayHelloPutMethod(@PathVariable String firstName,
+			@RequestParam(value = "lastName", defaultValue = "Hiremath") String lastName) {
+		return "Hello " + firstName + " " + lastName;
+	}
+}
