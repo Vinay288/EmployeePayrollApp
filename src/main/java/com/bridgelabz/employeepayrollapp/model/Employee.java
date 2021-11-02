63393c1f808873a3cc834fe5de801f02db45a059
Vinay288 <vinayhiremath288@gmail.com>
2021-11-02 12:46:09 +0530

    ADDED: employee model

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/Employee.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.employeepayrollapp.model;
+
+public class Employee {
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+}
