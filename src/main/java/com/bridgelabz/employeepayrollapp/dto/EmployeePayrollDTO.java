394fca91f333c36109bc06937c9d8af674808ecc
Vinay288 <vinayhiremath288@gmail.com>
2021-11-04 00:06:44 +0530

    ADDED: regex pattern for validation

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,9 +1,19 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+
 import lombok.Data;
 
 public @Data class EmployeePayrollDTO {
+	@NotNull
+	@Pattern(regexp="^[A-Z]{1}[a-zA-Z\\s]{2,}$",message="Employee Name is Invalid!")
 	public String name;
+	
+	@NotNull
+	@Min(value=500,message="Minimum wage should be more then 500")
 	public long salary;
 
 	public EmployeePayrollDTO(String name, long salary) {
