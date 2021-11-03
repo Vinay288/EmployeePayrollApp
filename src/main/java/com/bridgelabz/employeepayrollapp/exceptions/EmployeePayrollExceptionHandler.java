85afd75f2394ae03c0545e23227900228563796d
Vinay288 <vinayhiremath288@gmail.com>
2021-11-04 01:00:18 +0530

    ADDED: employee payroll exception handler

--- src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollExceptionHandler.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollExceptionHandler.java
@@ -15,13 +15,18 @@ import com.bridgelabz.employeepayrollapp.dto.ResponseDTO;
 @ControllerAdvice
 public class EmployeePayrollExceptionHandler {
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
-		List<ObjectError>errorList=exception.getBindingResult().getAllErrors();
-		List<String> errMsg=errorList.stream()
-							.map(objErr->objErr.getDefaultMessage())
-							.collect(Collectors.toList());
-		ResponseDTO response=new ResponseDTO("Exception while processing request", errMsg);
-		return new ResponseEntity<ResponseDTO>(response,HttpStatus.BAD_REQUEST);
+	public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(
+			MethodArgumentNotValidException exception) {
+		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+		List<String> errMsg = errorList.stream().map(objErr -> objErr.getDefaultMessage()).collect(Collectors.toList());
+		ResponseDTO response = new ResponseDTO("Exception while processing request", errMsg);
+		return new ResponseEntity<ResponseDTO>(response, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(EmployeePayrollException.class)
+	public ResponseEntity<ResponseDTO> handleEmployeePayrollException(EmployeePayrollException exception) {
+		ResponseDTO response = new ResponseDTO("Exception while processing request", exception.getMessage());
+		return new ResponseEntity<ResponseDTO>(response, HttpStatus.BAD_REQUEST);
 	}
 
 }
