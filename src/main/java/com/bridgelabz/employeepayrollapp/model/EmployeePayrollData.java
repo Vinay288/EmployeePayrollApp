9c6ee46e15b689c897e75b22cd54cb5b973a44f9
Vinay288 <vinayhiremath288@gmail.com>
2021-11-07 14:52:34 +0530

    ADDED:all employee attributes

--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -1,5 +1,8 @@
 package com.bridgelabz.employeepayrollapp.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 
 import lombok.Data;
@@ -8,6 +11,11 @@ public @Data class EmployeePayrollData {
 	private int employeeId;
 	private String name;
 	private long salary;
+	private String gender;
+	private LocalDate startDate;
+	private String note;
+	private String profilePic;
+	private List<String> departments;
 
 	public EmployeePayrollData() {
 	}
@@ -16,5 +24,10 @@ public @Data class EmployeePayrollData {
 		this.employeeId = employeeId;
 		this.name = employeePayrollDTO.name;
 		this.salary = employeePayrollDTO.salary;
+		this.gender = employeePayrollDTO.gender;
+		this.startDate = employeePayrollDTO.startDate;
+		this.note = employeePayrollDTO.note;
+		this.profilePic = employeePayrollDTO.profilePic;
+		this.departments = employeePayrollDTO.departments;
 	}
 }
