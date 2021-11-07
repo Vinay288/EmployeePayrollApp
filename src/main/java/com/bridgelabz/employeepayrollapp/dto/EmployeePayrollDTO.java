0798ab46f921f1304cd0841658ff12a5dd0a1e84
Vinay288 <vinayhiremath288@gmail.com>
2021-11-07 17:06:33 +0530

    ADDED: validation for all employee dto attributes

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Data;
 
@@ -18,9 +21,21 @@ public @Data class EmployeePayrollDTO {
 	@Min(value = 500, message = "Minimum wage should be more then 500")
 	public long salary;
 
+	@NotNull
+	@Pattern(regexp = "male|female", message = "gender should be male or female")
 	public String gender;
+
+	@JsonFormat(pattern = "dd MMM yyyy")
+	@NotNull(message = "Start Date should not be Null")
+	@PastOrPresent(message = "start date should be past or todays date")
 	public LocalDate startDate;
+
+	@NotNull(message = "note should not be Null")
 	public String note;
+
+	@NotNull(message = "profile pic should not be Null")
 	public String profilePic;
+
+	@NotNull(message = "department list should not be Null")
 	public List<String> departments;
 }
