9204e545bca906acc32985178f34842d5979d79f
Vinay288 <vinayhiremath288@gmail.com>
2021-11-07 14:52:04 +0530

    ADDED:all employee attributes

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,24 +1,26 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-
 
 import lombok.Data;
 
 public @Data class EmployeePayrollDTO {
 	@NotNull
-	@Pattern(regexp="^[A-Z]{1}[a-zA-Z\\s]{2,}$",message="Employee Name is Invalid!")
+	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee Name is Invalid!")
 	public String name;
-	
+
 	@NotNull
-	@Min(value=500,message="Minimum wage should be more then 500")
+	@Min(value = 500, message = "Minimum wage should be more then 500")
 	public long salary;
 
-	public EmployeePayrollDTO(String name, long salary) {
-		this.name = name;
-		this.salary = salary;
-	}
-
+	public String gender;
+	public LocalDate startDate;
+	public String note;
+	public String profilePic;
+	public List<String> departments;
 }
